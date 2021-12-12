1367210cf5e06d3ffa723e4f9ed30d6075e9f2b3
Mihran Mashhud <mihranmashhud@gmail.com>
2021-12-12 09:03:58 -0500

    Remove comment.

--- lua/configs/iris.lua
+++ lua/configs/iris.lua
@@ -21,23 +21,3 @@ require'iris'.setup{
     }
   }
 }
-        -- \ 'black':      ['#06080a',   '237',  'DarkGrey'],
-        -- \ 'bg0':        ['#1a1b26',   '235',  'Black'],
-        -- \ 'bg1':        ['#232433',   '236',  'DarkGrey'],
-        -- \ 'bg2':        ['#2a2b3d',   '236',  'DarkGrey'],
-        -- \ 'bg3':        ['#32344a',   '237',  'DarkGrey'],
-        -- \ 'bg4':        ['#3b3d57',   '237',  'Grey'],
-        -- \ 'bg_red':     ['#ff7a93',   '203',  'Red'],
-        -- \ 'diff_red':   ['#803d49',   '52',   'DarkRed'],
-        -- \ 'bg_green':   ['#b9f27c',   '107',  'Green'],
-        -- \ 'diff_green': ['#618041',   '22',   'DarkGreen'],
-        -- \ 'bg_blue':    ['#7da6ff',   '110',  'Blue'],
-        -- \ 'diff_blue':  ['#3e5380',   '17',   'DarkBlue'],
-        -- \ 'fg':         ['#a9b1d6',   '250',  'White'],
-        -- \ 'red':        ['#F7768E',   '203',  'Red'],
-        -- \ 'orange':     ['#FF9E64',   '215',  'Orange'],
-        -- \ 'yellow':     ['#E0AF68',   '179',  'Yellow'],
-        -- \ 'green':      ['#9ECE6A',   '107',  'Green'],
-        -- \ 'blue':       ['#7AA2F7',   '110',  'Blue'],
-        -- \ 'purple':     ['#ad8ee6',   '176',  'Magenta'],
-        -- \ 'grey':       ['#444B6A
