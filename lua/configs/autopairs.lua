27f272e234ff0e902c91241107d43e5f264c0938
Mihran Mashhud <mihranmashhud@gmail.com>
2022-12-27 14:59:12 -0500

    Update autopairs config.

--- lua/configs/autopairs.lua
+++ lua/configs/autopairs.lua
@@ -1,9 +1,11 @@
 local npairs = require'nvim-autopairs'
+
 npairs.setup {
   map_cr = true,
   map_complete = true,
   auto_select = false
 }
+
 npairs.add_rules(require('nvim-autopairs.rules.endwise-elixir'))
 npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
 npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
