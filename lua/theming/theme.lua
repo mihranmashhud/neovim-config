16b825b89d0288faa56010f83e666ccfa1f1ea0d
Mihran Mashhud <mihranmashhud@gmail.com>
2022-12-27 15:08:11 -0500

    Change default theme.

--- lua/theming/theme.lua
+++ lua/theming/theme.lua
@@ -1,8 +1,11 @@
 vim.g.tokyonight_transparent_background = 1
 vim.g.tokyonight_style = 'night'
 
+local theme = require'last-color'.recall() or 'kat.nvim'
+
+vim.cmd(("colorscheme %s"):format(theme))
+
 vim.cmd[[
-colorscheme tokyonight
 highlight VertSplit ctermbg=None guibg=None
 highlight EndOfBuffer ctermbg=None guibg=None
 
@@ -22,3 +25,6 @@ highlight GitGutterDeleteInvisible ctermbg=None guibg=None
 " Pmenu
 highlight Pmenu ctermbg=None guibg=None
 ]]
+
+vim.api.nvim_command('hi StatusLine guibg='..(require'galaxyline.theme'.default.bg))
+vim.api.nvim_set_hl(0, "TSGroup", { link = "@group"})
