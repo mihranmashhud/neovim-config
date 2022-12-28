0edd1fccb6574b7f7259f33337c27310647b5332
Mihran Mashhud <mihranmashhud@gmail.com>
2022-12-27 21:04:53 -0500

    remove eslint diagnostics.

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,15 +4,15 @@ local null_ls = require 'null-ls'
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.code_actions.eslint, null_ls.builtins.diagnostics.eslint,
-
+    null_ls.builtins.code_actions.eslint,
     null_ls.builtins.formatting.prettier.with({
       filetypes = {
         "javascript", "javascriptreact", "typescript", "typescriptreact", "vue",
         "css", "scss", "less", "html", "json", "jsonc", "yaml", "markdown",
         "graphql", "handlebars", "svelte"
       }
-    }), null_ls.builtins.formatting.bibclean,
+    }),
+    null_ls.builtins.formatting.bibclean,
     null_ls.builtins.formatting.lua_format.with({
       extra_args = {"--indent-width=2", "--tab-width=2"}
     })
