96f35df400ef59efca2d86545c9d030ad656e79a
Mihran Mashhud <mihranmashhud@gmail.com>
2022-12-27 21:24:33 -0500

    remove highlight commands that nvim-transparent takes care of.

--- lua/theming/theme.lua
+++ lua/theming/theme.lua
@@ -4,26 +4,4 @@ vim.g.tokyonight_style = "night"
 local theme = require"last-color".recall() or "kat.nvim"
 
 vim.cmd(("colorscheme %s"):format(theme))
-
-vim.cmd[[
-highlight VertSplit ctermbg=None guibg=None
-highlight EndOfBuffer ctermbg=None guibg=None
-
-" Git gutter
-highlight GitGutterAdd ctermfg=None guibg=None
-highlight GitGutterChange ctermbg=None guibg=None
-highlight GitGutterDelete ctermbg=None guibg=None
-highlight GitGutterChangeDelete ctermbg=None guibg=None
-highlight GitGutterAddLine ctermbg=None guibg=None
-highlight GitGutterChangeLine ctermbg=None guibg=None
-highlight GitGutterDeleteLine ctermbg=None guibg=None
-highlight GitGutterChangeDeleteLine ctermbg=None guibg=None
-highlight GitGutterAddInvisible ctermbg=None guibg=None
-highlight GitGutterChangeInvisible ctermbg=None guibg=None
-highlight GitGutterDeleteInvisible ctermbg=None guibg=None
-
-" Pmenu
-highlight Pmenu ctermbg=None guibg=None
-]]
-
 vim.api.nvim_set_hl(0, "TSGroup", { link = "@group"})
