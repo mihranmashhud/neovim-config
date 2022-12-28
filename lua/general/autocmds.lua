9c30af9e500aa80b8369023a8309dc91963d3068
Mihran Mashhud <mihranmashhud@gmail.com>
2022-12-27 20:36:50 -0500

    refactor: Switch to lua autocmd API.

--- lua/general/autocmds.lua
+++ lua/general/autocmds.lua
@@ -1,3 +1,10 @@
-local autocmd = require'utils.autocmd'.autocmd
-
-autocmd('TextYankPost * silent! lua vim.highlight.on_yank()')
+local group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
+vim.api.nvim_create_autocmd(
+  "TextYankPost",
+  {
+    callback = function() 
+      vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+    end,
+    group = group,
+  }
+)
