5538db235e0857dbcc82b4e683ec69ae16cbd679
Mihran Mashhud <mihranmashhud@gmail.com>
2022-12-29 16:54:50 -0500

    Remove unused settings.

--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -2,9 +2,6 @@ local autocmd_utils = require('utils/autocmd')
 local autocmd = autocmd_utils.autocmd
 local nnoremap = require'utils.map'.nnoremap
 
-vim.g.md_pdf_viewer = 'zathura'
-vim.g.md_args='--filter pandoc-citeproc -V --listings'
-
 autocmd('BufNewfile,BufRead *.Rmd set filetype=markdown')
 autocmd('BufNewfile,BufRead *.md set filetype=markdown')
 autocmd('BufNewfile,BufRead *.md set spell')
