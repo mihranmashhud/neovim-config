ab267b7590dcb65f27c7cfdaa5f46eb9fb8e426c
Mihran Mashhud <mihranmashhud@gmail.com>
2023-10-13 21:34:05 -0400

    Update to newer indentline settings.

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,7 +1,6 @@
-require"indent_blankline".setup{
-  show_current_context = true,
-  buftype_exclude = { "terminal", "nofile", "quickfix", "prompt" },
-  filetype_exclude = { "help", "alpha", "neo-tree", "lazy" },
-  use_treesitter = true,
-  show_first_indent_level = false,
+require"ibl".setup{
+  exclude = {
+    filetypes = { "help", "alpha", "neo-tree", "lazy" },
+    buftypes = { "terminal", "nofile", "quickfix", "prompt" },
+  }
 }
