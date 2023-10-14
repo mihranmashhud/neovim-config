24b84c705ddaa53af2f1616257859edd90cef428
Mihran Mashhud <mihranmashhud@gmail.com>
2023-10-13 21:34:30 -0400

    Fix some transparency issues.

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,3 +1,12 @@
-require"transparent".setup{
-  extra_groups = { "TelescopeBorder" },
+local transparent = require"transparent"
+transparent.setup{
+  extra_groups = { 
+    "NormalFloat",
+    "TelescopeBorder",
+    "FloatermBorder",
+    "WhichKeyBorder",
+    "FloatBorder",
+  },
 }
+transparent.clear_prefix("GitSigns")
+transparent.clear_prefix("Diagnostic")
