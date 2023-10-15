2521d4cc54ee51fbcfd1827cd9ef066109950ea5
Mihran Mashhud <mihranmashhud@gmail.com>
2023-10-15 17:47:40 -0400

    Change kitty background to match colorscheme.

--- lua/general/commands.lua
+++ lua/general/commands.lua
@@ -14,3 +14,28 @@ autocmd("TextYankPost", {
   end,
   group = group,
 })
+
+-- Set kitty background to colorscheme background
+group = augroup("kitty_background", { clear = true })
+
+local set_kitty_bg = function(bg)
+  vim.fn.system("kitty @ set-colors background='"..bg.."'")
+end
+local kitty_bg = vim.fn.system("kitty @ get-colors | grep '^background' | tr -s ' ' | cut -d' ' -f2")
+kitty_bg = kitty_bg:sub(1,7)
+autocmd("VimLeavePre", {
+  callback = function()
+    set_kitty_bg(kitty_bg)
+  end,
+  group = group,
+})
+autocmd("ColorScheme", {
+  callback = function()
+    local bg = vim.api.nvim_get_hl(0,{name="Normal"}).bg
+    if bg then
+      bg = "#"..string.format("%x", bg)
+      set_kitty_bg(bg)
+    end
+  end,
+  group = group,
+})
